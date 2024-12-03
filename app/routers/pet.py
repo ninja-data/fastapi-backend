06b46cafdc18ba8f1c3fd24b536ea9643be90ee7
Nijat <abdullayevnicat596@gmail.com>
2024-12-03 22:13:37 +0400

    feat: Add endpoints to get and delete a pet

--- app/routers/pet.py
+++ app/routers/pet.py
@@ -1,4 +1,4 @@
-from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi import Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.sql import func
 from sqlalchemy.orm import Session
 from typing import List, Optional
@@ -12,7 +12,7 @@ router =APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.PetOut])
-async def get_posts(db:Session = Depends(get_db), current_user: dict = Depends(oauth2.get_current_user),
+async def get_pets(db:Session = Depends(get_db), # current_user: dict = Depends(oauth2.get_current_user),
                     limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # TODO Show own posts only
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
@@ -30,3 +30,37 @@ async def create_pet(pet: schemas.PetCreate, db: Session = Depends(get_db), curr
     db.commit()
     db.refresh(new_pet)
     return new_pet
+
+
+@router.get("/{id}", response_model=schemas.PetOut)
+async def get_pet(id: int, db: Session = Depends(get_db)):
+
+    pet = db.query(models.Pet).filter(models.Pet.id == id).first()
+
+    if not pet:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"pet with id: {id} was not found")
+    
+    return pet
+
+
+@router.delete("/id", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_pet(id: int, db: Session = Depends(get_db), current_user: dict = Depends(oauth2.get_current_user)):
+    pet_query = db.query(models.Pet).filter(models.Pet.id == id)
+
+    pet = pet_query.first()
+
+    if pet == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"pet with id {id} does not exist")
+    
+    if pet.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
+    
+    pet_query.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# TODO add put
